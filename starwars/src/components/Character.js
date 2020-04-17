8cc9043dd390095e658208bb24d5451c41944fa2
Angelg10 <angelguzman97@gmail.com>
2020-04-17 11:16:45 -0400

    added components, state, props, axios, and styled components

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,20 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
 // Write your Character component here
+
+const baseURL = 'https://swapi.py4e.com/api/'
+
+const peopleURL = 'https://swapi.py4e.com/api/people/'
+
+function Character ({people, setPeople}) {
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
+export default Character;
